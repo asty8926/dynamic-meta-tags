541c5c9a8105c176b13c2ae40a54542628d8dd3e
Asty <31024172+asty8926@users.noreply.github.com>
2022-04-13 18:29:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-let meta1 = document.createElement("meta")
-meta1.setAttribute("property", "twitter:title")
-meta1.setAttribute("content", "Testing")
+let twitterTitle = document.createElement("meta")
+twitterTitle.setAttribute("property", "twitter:title")
+twitterTitle.setAttribute("content", "Twitter Title")
 
-let meta2 = document.createElement("meta")
-meta2.setAttribute("name", "twitter:card")
-meta2.setAttribute("content", "app")
+let twitterCard = document.createElement("meta")
+twitterCard.setAttribute("name", "twitter:card")
+twitterCard.setAttribute("content", "app")
 
-let meta3 = document.createElement("meta")
-meta3.setAttribute("name", "twitter:description")
-meta3.setAttribute("content", "Test Description")
+let twitterDesc = document.createElement("meta")
+twitterDesc.setAttribute("name", "twitter:description")
+twitterDesc.setAttribute("content", "Twitter Description")
+
+let ogTitle = document.createElement("meta")
+ogTitle.setAttribute("property", "og:title")
+ogTitle.setAttribute("content", "OG Title")
+
+let ogDesc = document.createElement("meta")
+ogDesc.setAttribute("property", "og:description")
+ogDesc.setAttribute("content", "OG Description")
 
 
-document.head.appendChild(meta1)
-document.head.appendChild(meta2)
-document.head.appendChild(meta3)
+document.head.appendChild(twitterTitle)
+document.head.appendChild(twitterCard)
+document.head.appendChild(twitterDesc)
+document.head.appendChild(ogTitle)
+document.head.appendChild(ogDesc)
