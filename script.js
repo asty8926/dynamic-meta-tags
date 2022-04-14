d56bd6fa998992baf2cd442be6c526ddfa67d596
Asty <31024172+asty8926@users.noreply.github.com>
2022-04-14 12:06:24 +0200

    Implemented self-executing loop function

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-let twitterTitle = document.createElement("meta")
-twitterTitle.setAttribute("property", "twitter:title")
-twitterTitle.setAttribute("content", "Twitter Title")
+let metas = {
+	twitterTitle: {
+		property: "twitter:title",
+		content: "Twitter Title"
+	},
+	twitterCard: {
+		name: "twitter:card",
+		content: "app"
+	},
+	twitterDesc: {
+		name: "twitter:description",
+		content: "Twitter Description"
+	},
+	ogTitle: {
+		property: "og:title",
+		content: "OG Title"
+	},
+	ogDescription: {
+		property: "og:description",
+		content: "OG Description"
+	}
+}
 
-let twitterCard = document.createElement("meta")
-twitterCard.setAttribute("name", "twitter:card")
-twitterCard.setAttribute("content", "app")
+// function that executes itself as soon as it's created
+(function generateMetas(){
+	for (const [metaName, metaObject] of Object.entries(metas)) {
+		let metaEl = document.createElement("meta")
+		metaEl.setAttribute(Object.entries(metaObject)[0][0], Object.entries(metaObject)[0][1])
+		metaEl.setAttribute(Object.entries(metaObject)[1][0], Object.entries(metaObject)[1][1])
 
-let twitterDesc = document.createElement("meta")
-twitterDesc.setAttribute("name", "twitter:description")
-twitterDesc.setAttribute("content", "Twitter Description")
-
-let ogTitle = document.createElement("meta")
-ogTitle.setAttribute("property", "og:title")
-ogTitle.setAttribute("content", "OG Title")
-
-let ogDesc = document.createElement("meta")
-ogDesc.setAttribute("property", "og:description")
-ogDesc.setAttribute("content", "OG Description")
-
-
-document.head.appendChild(twitterTitle)
-document.head.appendChild(twitterCard)
-document.head.appendChild(twitterDesc)
-document.head.appendChild(ogTitle)
-document.head.appendChild(ogDesc)
+		document.head.appendChild(metaEl)
+	}
+})()
